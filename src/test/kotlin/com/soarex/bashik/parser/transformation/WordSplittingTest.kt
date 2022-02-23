28540ce5c15bd1fddd0ba8c80e488628b890fe89
soarex <soarex16@gmail.com>
2022-02-23 21:50:06 +0300

    Add WordSplitting

---
+++ src/test/kotlin/com/soarex/bashik/parser/transformation/WordSplittingTest.kt
@@ -0,0 +1,122 @@
+package com.soarex.bashik.parser.transformation
+
+import com.soarex.bashik.parser.lexer.Token
+import com.soarex.bashik.parser.lexer.WordToken
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import kotlin.test.assertContentEquals
+
+internal class WordSplittingTest {
+
+    @ParameterizedTest
+    @MethodSource("transformTestData")
+    fun transform(separators: String, inputSequence: Sequence<Token>, expectedRefinedSequence: Sequence<Token>) {
+        val transform = WordSplitting(separators)
+
+        val transformationResult = transform.transform(inputSequence)
+
+        assertContentEquals(expectedRefinedSequence, transformationResult)
+    }
+
+    companion object {
+        @JvmStatic
+        fun transformTestData() = listOf(
+            Arguments.of(
+                " \t\n",
+                sequenceOf(
+                    WordToken("abc def"),
+                ),
+                sequenceOf(
+                    WordToken("abc"),
+                    WordToken("def"),
+                ),
+            ),
+            Arguments.of(
+                " \t\n",
+                sequenceOf(
+                    WordToken("abc"),
+                ),
+                sequenceOf(
+                    WordToken("abc"),
+                ),
+            ),
+            Arguments.of(
+                " \t\n",
+                sequenceOf(
+                    WordToken("'single quoted'  "),
+                ),
+                sequenceOf(
+                    WordToken("'single quoted'"),
+                ),
+            ),
+            Arguments.of(
+                " \t\n",
+                sequenceOf(
+                    WordToken("\"double quoted\"  "),
+                ),
+                sequenceOf(
+                    WordToken("\"double quoted\""),
+                ),
+            ),
+            Arguments.of(
+                " \t\n",
+                sequenceOf(
+                    WordToken("\"mixed quotes\'  "),
+                    WordToken("\'mixed quotes\"  "),
+                ),
+                sequenceOf(
+                    // Эти слова некорректны (непарные кавычки), поэтому не будут обработаны
+                    WordToken("\"mixed quotes\'"),
+                    WordToken("\'mixed quotes\""),
+                ),
+            ),
+            Arguments.of(
+                " \t\n",
+                sequenceOf(
+                    WordToken("\"mixed quotes'    'some text\""),
+                ),
+                sequenceOf(
+                    WordToken("\"mixed quotes'    'some text\""),
+                ),
+            ),
+            Arguments.of(
+                " \t\n",
+                sequenceOf(
+                    WordToken("  "),
+                ),
+                emptySequence<Token>(),
+            ),
+            Arguments.of(
+                " \t\n",
+                sequenceOf(
+                    WordToken("\n\n\n"),
+                ),
+                emptySequence<Token>(),
+            ),
+            Arguments.of(
+                " \t\n",
+                sequenceOf(
+                    WordToken("multi\nline\ntext"),
+                ),
+                sequenceOf(
+                    WordToken("multi"),
+                    WordToken("line"),
+                    WordToken("text"),
+                ),
+            ),
+            Arguments.of(
+                " \t\n",
+                sequenceOf(
+                    WordToken("multiple\n\n\nseparators\t\t\tat   once"),
+                ),
+                sequenceOf(
+                    WordToken("multiple"),
+                    WordToken("separators"),
+                    WordToken("at"),
+                    WordToken("once"),
+                ),
+            ),
+        )
+    }
+}
