1b2c33195e1c125f44b9e83a528b07d887678943
soarex <soarex16@gmail.com>
2022-03-02 22:45:15 +0300

    Fix imports in Environment tests

--- src/test/kotlin/com/soarex/bashik/EnvironmentKtTest.kt
+++ src/test/kotlin/com/soarex/bashik/EnvironmentKtTest.kt
@@ -1,5 +1,7 @@
 package com.soarex.bashik
 
+import com.soarex.bashik.runtime.ProcessContext
+import com.soarex.bashik.runtime.ProcessContextImpl
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
